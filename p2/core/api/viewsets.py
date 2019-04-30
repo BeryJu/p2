1aae1981d35e4dd8324d92bca817ee50b7e1620d
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-04-30 17:01:32 +0200

    add ugly but working image uploader

--- p2/core/api/viewsets.py
+++ p2/core/api/viewsets.py
@@ -1,5 +1,7 @@
 """Core API Viewsets"""
+from django.http import Http404
 from drf_yasg.utils import swagger_auto_schema
+from guardian.shortcuts import assign_perm, get_objects_for_user
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -43,6 +45,32 @@ class VolumeViewSet(ModelViewSet):
     queryset = Volume.objects.all()
     serializer_class = VolumeSerializer
 
+    # @swagger_auto_schema(method='POST', responses={
+    #     '200': BlobPayloadSerializer()
+    # })
+    @action(detail=True, methods=['post'])
+    # pylint: disable=invalid-name
+    def upload(self, request, pk=None):
+        """Create blob from HTML Form upload"""
+        volumes = get_objects_for_user(request.user, 'p2_core.use_volume')
+        if not volumes.exists():
+            raise Http404
+        volume = volumes.first()
+        count = 0
+        # TODO: Check blob create permissions
+        for key in request.FILES:
+            file = request.FILES[key]
+            blob = Blob.objects.create(
+                path=file.name,
+                volume=volume,
+                payload=file.read()
+            )
+            # assign permission to blob
+            assign_perm('view_blob', request.user, blob)
+            count += 1
+        return Response({
+            'count': count
+        })
 
 class StorageViewSet(ModelViewSet):
     """
