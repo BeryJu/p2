20bda5975eb730caf2947c881ec87fabcb7b1298
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-05-03 10:41:56 +0200

    make ComponentForm usable without having to subclass it

--- p2/core/components/forms.py
+++ p2/core/components/forms.py
@@ -4,6 +4,13 @@ from django import forms
 from p2.core.models import Component
 
 
+# pylint: disable=too-few-public-methods
+class ComponentFormMeta:
+    """Base meta class, set common fields"""
+
+    model = Component
+    fields = ['enabled']
+
 class ComponentForm(forms.ModelForm):
     """Base form for all Component forms, adds a load(self, instance) method to fill fields"""
 
@@ -11,9 +18,5 @@ class ComponentForm(forms.ModelForm):
         """Load self.fields[x] from instance.tags"""
         pass
 
-# pylint: disable=too-few-public-methods
-class ComponentFormMeta:
-    """Base meta class, set common fields"""
-
-    model = Component
-    fields = ['enabled']
+    class Meta(ComponentFormMeta):
+        pass
