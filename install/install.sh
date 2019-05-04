39eab7b7a42296a7bf0e16c8b8a5d6feb8bde171
Jens Langhammer <jens.langhammer@beryju.org>
2019-05-04 23:29:38 +0200

    start implementing cert-manager

--- install/install.sh
+++ install/install.sh
@@ -5,10 +5,10 @@
 # Supported enviormnet variables:
 # - INGRESS_HOST: Hostname under which p2 will be available
 # - STORAGE_BASE: Base directory in which p2 data will be storeed
+# - LE_MAIL: Optional; Let's Encrypt E-Mail. If this is not set, Let's Encrypt is not enabled.
 
 K3S_VERSION="0.4.0"
 P2_VERSION="0.1.16"
-export INSTALL_K3S_EXEC="--cluster-cidr 10.121.0.0/16 --cluster-domain p2.baked --docker"
 export INSTALL_K3S_EXEC="--cluster-cidr 10.121.0.0/16 --cluster-domain p2.baked --docker --no-deploy traefik"
 
 if [ "$EUID" -ne 0 ]; then
@@ -49,6 +49,7 @@ PASSWORD=$(cat $P2_PASSWORD_FILE)
 curl -fsSL -o p2_k3s_helm.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-helm.yaml"
 curl -fsSL -o p2_k3s_storage.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-storage.yaml"
 curl -fsSL -o p2_k3s_nginx.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-nginx-ingress.yaml"
+curl -fsSL -o p2_k3s_cert.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-cert-manager.yaml"
 
 # Replace variable in Helm CRD
 sed -i "s|%INGRESS_HOST%|${INGRESS_HOST}|g" p2_k3s_helm.yaml
@@ -58,9 +59,17 @@ sed -i "s|%PASSWORD%|${PASSWORD}|g" p2_k3s_helm.yaml
 sed -i "s|%P2_STORAGE_PATH%|${P2_STORAGE_PATH}|g" p2_k3s_storage.yaml
 sed -i "s|%P2_DATABASE_PATH%|${P2_DATABASE_PATH}|g" p2_k3s_storage.yaml
 
+# Replace variable in cert-manager
+sed -i "s|%LE_MAIL%|${LE_MAIL}|g" p2_k3s_cert.yaml
+
 mv p2_k3s_nginx.yaml /var/lib/rancher/k3s/server/manifests/p2-10-nginx.yaml
 mv p2_k3s_storage.yaml /var/lib/rancher/k3s/server/manifests/p2-20-storage.yaml
 mv p2_k3s_helm.yaml /var/lib/rancher/k3s/server/manifests/p2-30-helm.yaml
+
+# Only deploy cert-manager if LE_MAIL set
+if [ -n "$LE_MAIL" ]; then
+    mv p2_k3s_cert.yaml /var/lib/rancher/k3s/server/manifests/p2-40-cert.yaml
+fi
 
 echo " * Your p2 instanace will be available at $INGRESS_HOST in a few minutes."
 echo " * You can use the username admin with password admin to login."
