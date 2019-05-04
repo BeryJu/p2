d45712fea5c983b88bc0cfd1b8709e8af9d709c3
Jens Langhammer <jens.langhammer@beryju.org>
2019-05-04 23:29:26 +0200

    replace traefik with nginx ingress, add annotations to allow big reqeuests

--- install/install.sh
+++ install/install.sh
@@ -9,6 +9,7 @@
 K3S_VERSION="0.4.0"
 P2_VERSION="0.1.16"
 export INSTALL_K3S_EXEC="--cluster-cidr 10.121.0.0/16 --cluster-domain p2.baked --docker"
+export INSTALL_K3S_EXEC="--cluster-cidr 10.121.0.0/16 --cluster-domain p2.baked --docker --no-deploy traefik"
 
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
@@ -47,6 +48,7 @@ PASSWORD=$(cat $P2_PASSWORD_FILE)
 # Download Helm Chart CRD for k3s, replace values and install
 curl -fsSL -o p2_k3s_helm.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-helm.yaml"
 curl -fsSL -o p2_k3s_storage.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-storage.yaml"
+curl -fsSL -o p2_k3s_nginx.yaml "https://git.beryju.org/BeryJu.org/p2/raw/version/${P2_VERSION}/install/k3s-nginx-ingress.yaml"
 
 # Replace variable in Helm CRD
 sed -i "s|%INGRESS_HOST%|${INGRESS_HOST}|g" p2_k3s_helm.yaml
@@ -56,8 +58,9 @@ sed -i "s|%PASSWORD%|${PASSWORD}|g" p2_k3s_helm.yaml
 sed -i "s|%P2_STORAGE_PATH%|${P2_STORAGE_PATH}|g" p2_k3s_storage.yaml
 sed -i "s|%P2_DATABASE_PATH%|${P2_DATABASE_PATH}|g" p2_k3s_storage.yaml
 
-mv p2_k3s_storage.yaml /var/lib/rancher/k3s/server/manifests/p2-10-storage.yaml
-mv p2_k3s_helm.yaml /var/lib/rancher/k3s/server/manifests/p2-20-helm.yaml
+mv p2_k3s_nginx.yaml /var/lib/rancher/k3s/server/manifests/p2-10-nginx.yaml
+mv p2_k3s_storage.yaml /var/lib/rancher/k3s/server/manifests/p2-20-storage.yaml
+mv p2_k3s_helm.yaml /var/lib/rancher/k3s/server/manifests/p2-30-helm.yaml
 
 echo " * Your p2 instanace will be available at $INGRESS_HOST in a few minutes."
 echo " * You can use the username admin with password admin to login."
