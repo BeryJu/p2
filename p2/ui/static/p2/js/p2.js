83fb045c50591ef95c551617b6270056785a4814
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-05-09 17:54:37 +0200

    add Card to call re-index function

--- p2/ui/static/p2/js/p2.js
+++ p2/ui/static/p2/js/p2.js
@@ -1,23 +1,53 @@
-$('.dz').dropzone({
-    previewTemplate: '<div style="display:none"></div>',
-    init: function () {
-        this.on('error', function (file, errorMessage) {
-            dzPostUploadToast(errorMessage.detail, false);
-        });
-        this.on("success", function (file) {
-            dzPostUploadToast(file.name, true);
-        });
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
+    return cookieValue;
+}
+
+$('.dz').dropzone({
+  previewTemplate: '<div style="display:none"></div>',
+  init: function () {
+    this.on('error', function (file, errorMessage) {
+      dzPostUploadToast(errorMessage.detail, false);
+    });
+    this.on("success", function (file) {
+      dzPostUploadToast(file.name, true);
+    });
+  }
+});
+
+$('[data-api-url]').on('click', (e) => {
+  $(e.currentTarget).prepend(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`);
+  // Make the actual request
+  let headers = new Headers();
+  headers.append('X-CSRFToken', getCookie('csrftoken'));
+  let request = new Request($(e.currentTarget).data('api-url'));
+  fetch(request, {
+    method: 'POST',
+    headers: headers
+  }).then(response => {
+    $(e.currentTarget).find('span.spinner-border').remove();
+  });
 });
 
 const dzPostUploadToast = function (message, uploadSucceeded) {
-    let text = '';
-    if (uploadSucceeded) {
-        text = `Successfully uploaded ${message}.`;
-    } else {
-        text = `Failed to upload file: ${message}.`;
-    }
-    const template = `
+  let text = '';
+  if (uploadSucceeded) {
+    text = `Successfully uploaded ${message}.`;
+  } else {
+    text = `Failed to upload file: ${message}.`;
+  }
+  const template = `
         <div class="toast ml-auto" role="alert" data-delay="3000" data-autohide="true">
           <div class="toast-header">
             <strong class="mr-auto text-primary">Blob Upload</strong>
@@ -29,7 +59,7 @@ const dzPostUploadToast = function (message, uploadSucceeded) {
             ${text}
           </div>
         </div>`;
-    $('.alert-container').append(template);
-    // initialize and show Bootstrap 4 toast
-    $('.toast').toast('show');
+  $('.alert-container').append(template);
+  // initialize and show Bootstrap 4 toast
+  $('.toast').toast('show');
 }
