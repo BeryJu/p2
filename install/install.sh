f9006c055c4098d3f2c33a9b20cf63602bb81dc8
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-05-15 12:31:37 +0200

    make worker autoreload aswell

--- install/install.sh
+++ install/install.sh
@@ -64,6 +64,8 @@ sed -i "s|%STORAGE_BASE%|${STORAGE_BASE}|g" p2_k3s_storage.yaml
 # Replace variable in cert-manager
 # sed -i "s|%LE_MAIL%|${LE_MAIL}|g" p2_k3s_cert.yaml
 
+# TODO: Check if kubectl exists and if existing pods are running
+
 mv p2_k3s_nginx.yaml /var/lib/rancher/k3s/server/manifests/p2-10-nginx.yaml
 mv p2_k3s_storage.yaml /var/lib/rancher/k3s/server/manifests/p2-20-storage.yaml
 mv p2_k3s_helm.yaml /var/lib/rancher/k3s/server/manifests/p2-30-helm.yaml
@@ -73,7 +75,11 @@ mv p2_k3s_helm.yaml /var/lib/rancher/k3s/server/manifests/p2-30-helm.yaml
 #     mv p2_k3s_cert.yaml /var/lib/rancher/k3s/server/manifests/p2-40-cert.yaml
 # fi
 
+# TODO: If existing pods were running, delete them
+
 echo " * Your p2 instanace will be available at $INGRESS_HOST in a few minutes."
 echo " * You can use the username admin with password admin to login."
+
+# TODO: Wait for webinterface to become available
 
 rm -r "${TEMP_DIR}"
