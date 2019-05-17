8f607f8583672b0b665f1c191f7a63b0b3faeb5e
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-05-17 12:40:49 +0200

    add proper validation to volume name

--- p2/core/forms.py
+++ p2/core/forms.py
@@ -1,11 +1,26 @@
 """p2 core forms"""
 from django import forms
+from django.core.validators import RegexValidator
 
 from p2.core.constants import ATTR_BLOB_SIZE_BYTES
 from p2.core.models import Blob, Storage, Volume
 from p2.lib.forms import TagModelForm, TagModelFormMeta
 from p2.lib.reflection import path_to_class
 
+
+# pylint: disable=too-few-public-methods
+class VolumeValidator(RegexValidator):
+    """Validate volume name (s3-compatible)"""
+
+    regex = (r'(?=^.{3,63}$)(?!^(\d+\.)+\d+$)(^(([a-z0-9]|[a-z0-9][a-z0-9\-]'
+             r'*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$)')
+    message = """Volume names must be at least 3 and no more than 63 characters long.
+        Volume names must be a series of one or more labels.
+        Volume names can contain lowercase letters, numbers, and hyphens.
+        Each label must start and end with a lowercase letter or a number.
+        Adjacent labels are separated by a single period (.)
+        Volume names must not be formatted as an IP address (for example, 192.168.5.4)
+        """
 
 class BlobForm(TagModelForm):
     """blob form"""
@@ -58,10 +73,9 @@ class StorageForm(TagModelForm):
 class VolumeForm(TagModelForm):
     """volume form"""
 
+    name = forms.CharField(validators=[VolumeValidator()])
+
     class Meta(TagModelFormMeta):
 
         model = Volume
         fields = ['name', 'storage', 'tags']
-        widgets = {
-            'name': forms.TextInput
-        }
