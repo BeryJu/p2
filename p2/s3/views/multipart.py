4b84b65a68678e6e3c1bde5542a30f6f430acd42
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-05-17 11:12:47 +0200

    fix MultipartUploadComplete to not timeout anymore

--- p2/s3/views/multipart.py
+++ p2/s3/views/multipart.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from xml.etree import ElementTree
 
 from django.contrib.postgres.fields.jsonb import KeyTextTransform
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, StreamingHttpResponse
 from guardian.shortcuts import assign_perm, get_objects_for_user
 
 from p2.core.constants import ATTR_BLOB_HASH_MD5
@@ -52,40 +52,51 @@ class MultipartUploadView(S3Authentication):
             'tags__%s' % TAG_S3_MULTIPART_BLOB_TARGET_BLOB: path,
             'volume': volume
         })
-        # Create the destination blob
-        blobs = get_objects_for_user(request.user, 'p2_core.change_blob', Blob).filter(
-            path=path, volume=volume)
-        if not blobs.exists():
-            destination_blob = Blob.objects.create(
-                path=path,
-                volume=volume)
-            # We're creating a new blob, hence assign all default permissions
-            for permission in ['view_blob', 'change_blob', 'delete_blob']:
-                assign_perm('p2_core.%s' % permission, request.user, destination_blob)
-        else:
-            destination_blob = blobs.first()
-        # Go through all temporary blobs and combine them into one
-        try:
-            parts = get_objects_for_user(request.user, 'p2_core.change_blob').filter(**{
-                'tags__%s' % TAG_S3_MULTIPART_BLOB_TARGET_BLOB: path,
-                'tags__%s' % TAG_S3_MULTIPART_BLOB_UPLOAD_ID: upload_id,
-            })
-            # We need to annotate TAG_S3_MULTIPART_BLOB_PART so we can use order_by
-            parts = parts.annotate(part_number=KeyTextTransform(TAG_S3_MULTIPART_BLOB_PART, 'tags'))
-            parts = parts.order_by('part_number')
-            for part in parts:
-                destination_blob.write(part.read())
-            destination_blob.save()
-            parts.delete()
-        except BlobException as exc:
-            return XMLResponse(exc)
-        root = ElementTree.Element("{%s}CompleteMultipartUploadResult" % XML_NAMESPACE)
-        ElementTree.SubElement(root, "Location").text = "http://nope"
-        ElementTree.SubElement(root, "Bucket").text = volume.name
-        ElementTree.SubElement(root, "Key").text = path
-        ElementTree.SubElement(root, "ETag").text = \
-            destination_blob.attributes.get(ATTR_BLOB_HASH_MD5)
-        return XMLResponse(root)
+        def generator():
+            """Generator to merge all part-blobs together. Yields spaces while merging
+            is running to keep request from timing out"""
+            # Create the destination blob
+            blobs = get_objects_for_user(request.user, 'p2_core.change_blob', Blob).filter(
+                path=path, volume=volume)
+            if not blobs.exists():
+                destination_blob = Blob.objects.create(
+                    path=path,
+                    volume=volume)
+                # We're creating a new blob, hence assign all default permissions
+                for permission in ['view_blob', 'change_blob', 'delete_blob']:
+                    assign_perm('p2_core.%s' % permission, request.user, destination_blob)
+            else:
+                destination_blob = blobs.first()
+            yield '<?xml version="1.0" encoding="utf8"?>' # Yield XML Header
+            # Go through all temporary blobs and combine them into one
+            try:
+                parts = get_objects_for_user(request.user, 'p2_core.change_blob').filter(**{
+                    'tags__%s' % TAG_S3_MULTIPART_BLOB_TARGET_BLOB: path,
+                    'tags__%s' % TAG_S3_MULTIPART_BLOB_UPLOAD_ID: upload_id,
+                })
+                # We need to annotate TAG_S3_MULTIPART_BLOB_PART so we can use order_by
+                parts = parts.annotate(
+                    part_number=KeyTextTransform(TAG_S3_MULTIPART_BLOB_PART, 'tags'))
+                parts = parts.order_by('part_number')
+                for part in parts:
+                    destination_blob.write(part.read())
+                    yield ' ' # Yield space to keep connection alive
+                destination_blob.save()
+                yield ' '  # Yield space to keep connection alive
+                parts.delete()
+                yield ' '  # Yield space to keep connection alive
+            except BlobException as exc:
+                yield exc
+            yield ' '
+            root = ElementTree.Element("{%s}CompleteMultipartUploadResult" % XML_NAMESPACE)
+            ElementTree.SubElement(root, "Location").text = "http://nope"
+            ElementTree.SubElement(root, "Bucket").text = volume.name
+            ElementTree.SubElement(root, "Key").text = path
+            ElementTree.SubElement(root, "ETag").text = \
+                destination_blob.attributes.get(ATTR_BLOB_HASH_MD5)
+            # Return final response
+            yield ElementTree.tostring(root)
+        return StreamingHttpResponse(generator())
 
     def post_handle_mp_initiate(self, request, volume, path):
         """https://docs.aws.amazon.com/AmazonS3/latest/API/mpUploadInitiate.html"""
@@ -101,7 +112,7 @@ class MultipartUploadView(S3Authentication):
             blob = existing.first()
         else:
             blob = Blob.objects.create(
-                path='/%s' % uuid4().hex,
+                path='/%s/%d' % (uuid4().hex, 1),
                 volume=volume,
                 tags={
                     TAG_S3_MULTIPART_BLOB_PART: 1,
