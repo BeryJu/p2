e61cf9d0b197303354cb8d69effb01036b7cf36c
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-05-17 23:15:47 +0200

    suppress  ConnectionRefusedError

--- p2/lib/sentry.py
+++ p2/lib/sentry.py
@@ -19,6 +19,7 @@ def before_send(event, hint):
         ConnectionResetError,
         WorkerLostError,
         DisallowedHost,
+        ConnectionResetError,
     )
     if 'exc_info' in hint:
         _exc_type, exc_value, _ = hint['exc_info']
