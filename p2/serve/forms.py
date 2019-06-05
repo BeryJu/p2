fb4f748a03335df75783430f5284d8bf7c5d4a58
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-06-05 10:45:16 +0200

    Rewrite ServeRule Form's help text

--- p2/serve/forms.py
+++ p2/serve/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
+from p2 import __version__
 from p2.lib.forms import TagModelForm, TagModelFormMeta
 from p2.serve.models import ServeRule
 
@@ -19,12 +20,22 @@ class ServeRuleForm(TagModelForm):
             'blob_query': forms.TextInput,
         }
         help_texts = {
+            'tags': mark_safe(_("""
+            All of the used keys have to match the requests value for this Rule to trigger.
+            You can use Regular Expressions as values. See <a href=
+            "https://git.beryju.org/BeryJu.org/p2/blob/version/%(version)s/p2/serve/constants.py">
+            here</a> for a list of all possible tags.""" % {'version':__version__})),
             'blob_query': mark_safe(_("""
-            The placeholder '{path}' will be replaced with the request path.
-            '{host} will be replaced by the used hostname.
-            You can also use {meta[X]} where X is any of the fields
-            described <a href="https://docs.djangoproject.com/en/2.2/ref/request-response
-            /#django.http.HttpRequest.META">here</a>"""))
+            The following placeholders can be used:
+            <ul>
+                <li>{path} will be replaced by the full Request Path.
+                This path starts with a slash.</li>
+                <li>{path_relative} will be replaced by the relative Request Path.</li>
+                <li>{host} will be replaced by the requested Hostname.</li>
+                <li>{meta[X]} where X is any of the fields
+            described <a href=
+            "https://docs.djangoproject.com/en/2.2/ref/request-response/#django.http.HttpRequest.META">
+            here</a></li></ul>"""))
         }
 
 
