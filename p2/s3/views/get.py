a64d810d781f91e25970635d08f86e53d78cef0d
Jens Langhammer <jens.langhammer@beryju.org>
2019-06-09 21:48:17 +0200

    Improve S3 Compatibility by removing ns0: prefix in XML Responses

--- p2/s3/views/get.py
+++ p2/s3/views/get.py
@@ -4,6 +4,7 @@ from xml.etree import ElementTree
 from django.views import View
 from guardian.shortcuts import get_objects_for_user
 
+from p2.s3.constants import XML_NAMESPACE
 from p2.s3.http import XMLResponse
 
 
@@ -12,22 +13,18 @@ class ListView(View):
 
     def get(self, request):
         """Return list of Buckets"""
-        root = ElementTree.Element("{http://s3.amazonaws.com/doc/2006-03-01}ListAllMyBucketsResult")
+        root = ElementTree.Element("{%s}ListAllMyBucketsResult" % XML_NAMESPACE)
         owner = ElementTree.Element("Owner")
 
-        owner_id = ElementTree.SubElement(owner, 'ID')
-        owner_id.text = str(request.user.id)
-        owner_display_name = ElementTree.SubElement(owner, 'DisplayName')
-        owner_display_name.text = request.user.username
+        ElementTree.SubElement(owner, 'ID').text = str(request.user.id)
+        ElementTree.SubElement(owner, 'DisplayName').text = request.user.username
 
         buckets = ElementTree.Element('Buckets')
 
         for volume in get_objects_for_user(self.request.user, 'p2_core.use_volume'):
             bucket = ElementTree.Element("Bucket")
-            bucket_name = ElementTree.SubElement(bucket, "Name")
-            bucket_name.text = volume.name
-            bucket_creation_date = ElementTree.SubElement(bucket, "CreationDate")
-            bucket_creation_date.text = "2006-02-03T16:45:09.000Z"
+            ElementTree.SubElement(bucket, "Name").text = volume.name
+            ElementTree.SubElement(bucket, "CreationDate").text = "2006-02-03T16:45:09.000Z"
             buckets.append(bucket)
 
         root.append(owner)
