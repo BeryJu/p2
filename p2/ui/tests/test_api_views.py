ad4eef16a1d09a6e288bd2c3a119ffb9039a6681
Jens Langhammer <jens.langhammer@haufe-lexware.com>
2019-06-27 13:57:02 +0200

    p2 fix dev requirements

--- p2/ui/tests/test_api_views.py
+++ p2/ui/tests/test_api_views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from django.test import TestCase
+
 # from guardian.shortcuts import assign_perm
 
 # from p2.api.forms import APIKeyForm
