732aed1d5de0684755ef2c5a8a90c0912ff4cdc4
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-07-18 17:12:27 +0200

    ui: cache global blob coount

---
+++ p2/ui/constants.py
@@ -0,0 +1,3 @@
+"""p2 ui constants"""
+
+CACHE_KEY_BLOB_COUNT = 'cache.p2.io/ui/blob-count'
--- p2/ui/views/general.py
+++ p2/ui/views/general.py
@@ -1,9 +1,11 @@
 """p2 UI Index view"""
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
 from django.views.generic import ListView, TemplateView
 from guardian.shortcuts import get_objects_for_user
 
 from p2.core.models import Blob
+from p2.ui.constants import CACHE_KEY_BLOB_COUNT
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -11,9 +13,16 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
     template_name = 'general/index.html'
 
+    def get_blob_count(self):
+        """Get cached Blob Count"""
+        if not cache.get(CACHE_KEY_BLOB_COUNT, None):
+            count = len(Blob.objects.all())
+            cache.set(CACHE_KEY_BLOB_COUNT, count, 30)
+        return cache.get(CACHE_KEY_BLOB_COUNT)
+
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['count'] = len(Blob.objects.all())
+        data['count'] = self.get_blob_count()
         data['volumes'] = get_objects_for_user(self.request.user, 'p2_core.view_volume')
         return data
 
