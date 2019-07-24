46bae6238686191199f1070c592dd7445f28526e
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-07-24 16:53:07 +0200

    core: disable auto-reload for work (caused zombie processes)

--- p2/core/management/commands/worker.py
+++ p2/core/management/commands/worker.py
@@ -3,7 +3,6 @@
 from logging import getLogger
 
 from django.core.management.base import BaseCommand
-from django.utils import autoreload
 
 from p2.core.celery import CELERY_APP
 
@@ -15,9 +14,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """celery worker"""
-        autoreload.run_with_reloader(self.celery_worker)
-
-    def celery_worker(self):
-        """Run celery worker within autoreload"""
-        autoreload.raise_last_exception()
         CELERY_APP.worker_main(['worker', '--autoscale=10,3', '-E', '-B'])
