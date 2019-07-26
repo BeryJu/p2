ecdb3948914aecf1bda6359f900025b718478440
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-07-26 15:23:02 +0200

    s3: validate canned ACL if given

--- p2/s3/views/common.py
+++ p2/s3/views/common.py
@@ -6,10 +6,19 @@ from logging import getLogger
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from p2.s3.errors import AWSBadDigest, AWSInvalidDigest
+from p2.s3.errors import AWSBadDigest, AWSError, AWSInvalidDigest
 
 CONTENT_MD5_HEADER = 'HTTP_CONTENT_MD5'
+X_AMZ_ACL_HEADER = 'HTTP_X_AMZ_ACL'
 LOGGER = getLogger(__name__)
+
+VALID_ACLS = ["private",
+              "public-read",
+              "public-read-write",
+              "aws-exec-read",
+              "authenticated-read",
+              "bucket-owner-read",
+              "bucket-owner-full-control"]
 
 class S3View(View):
     """Base View for all S3 Views. Checks for common Headers and does database lookups."""
@@ -30,7 +39,16 @@ class S3View(View):
                              self.request.META.get(CONTENT_MD5_HEADER), ours)
                 raise AWSBadDigest
 
+    def apply_acl_permissions(self):
+        """Parse x-amz-acl Header into p2 permissions, returned as List"""
+        header = self.request.META.get(X_AMZ_ACL_HEADER)
+        if not header:
+            return
+        if header not in VALID_ACLS:
+            raise AWSError
+
     @csrf_exempt
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
         self._check_content_md5()
+        self.apply_acl_permissions()
